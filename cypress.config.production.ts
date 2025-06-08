ac237ec36bc65d314472fa6b180cd73afce2ca5d
HyunWoo Jeong <jrwedo@naver.com>
2025-06-08 20:31:42 +0900

    Setting: prod 환경 cypress url 변경

--- cypress.config.production.ts
+++ cypress.config.production.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://test.kakaotech.com',
+    baseUrl: 'https://cadev.kakaotech.com',
     setupNodeEvents(on, config) {
     },
     specPattern: ['**/*.cy.ts', '**/*.cy.tsx'],
     supportFile: false,
     env: {
-      API_URL: "http://test.kakaotech.com/api",
+      API_URL: "https://cadev.kakaotech.com/api",
     }
   },
 });
