3d9a54337ff35803b2a397c5b03a4acd95c380fc
HyunWoo Jeong <jrwedo@naver.com>
2025-06-10 16:01:13 +0900

    Feat: cypress에서 로그인 후 유저 데이터 저장

--- test/support/utils/auth.ts
+++ test/support/utils/auth.ts
@@ -1,5 +1,7 @@
+import AUTWindow = Cypress.AUTWindow;
+
 const login = async () => {
-  return await fetch('/login', {
+  return await fetch(Cypress.env('API_URL') + '/v1/auth/login', {
     method: 'POST',
     body: JSON.stringify({
       email: Cypress.env('TEST_EMAIL'),
@@ -12,13 +14,13 @@ const login = async () => {
 };
 
 const signup = async () => {
-  return await fetch('/auth/signup', {
+  return await fetch(Cypress.env('API_URL') + '/v1/auth/signup', {
     method: 'POST',
     body: JSON.stringify({
       email: Cypress.env('TEST_EMAIL'),
       password: Cypress.env('TEST_PASSWORD'),
       nickname: 'test',
-      apiKey: Cypress.env('TEST_API_KEY'),
+      apiKey: Cypress.env('API_KEY'),
     }),
     headers: {
       'Content-Type': 'application/json',
@@ -26,12 +28,26 @@ const signup = async () => {
   });
 };
 
+const setLogin = (win: AUTWindow, accessToken: string) => {
+  const authState = {
+    user: null,
+    isLoggedIn: true,
+    isApproved: true,
+  };
+
+  win.localStorage.setItem('auth', JSON.stringify(authState));
+  win.document.cookie = `ACCESS_TOKEN=${accessToken}; path=/;`;
+};
+
 // 로그인 → 실패 시 회원가입 → 재로그인
-export const loginWithFallbackSignup = async () => {
+export const loginWithFallbackSignup = async (win: AUTWindow) => {
   const loginRes = await login();
 
   if (loginRes.ok) {
-    return; // 로그인 성공
+    const body = await loginRes.json();
+    const token = body.data.accessToken;
+    setLogin(win, token);
+    return;
   }
 
   const signupRes = await signup();
@@ -40,7 +56,13 @@ export const loginWithFallbackSignup = async () => {
   }
 
   const retryLoginRes = await login();
+  const body = await retryLoginRes.json();
+  const token = body.data.accessToken;
+  setLogin(win, token);
+
   if (!retryLoginRes.ok) {
     throw new Error('로그인 재시도 실패');
   }
 };
+
+
