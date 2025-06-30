a0490f01351ed777e031abb0de91f40b61f73bb3
Changsu Park <gd12091@gmail.com>
2025-06-30 10:44:34 +0900

    🐛 fix: Docker image name

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -6,7 +6,7 @@ aws ecr get-login-password --region ap-northeast-2 | \
 docker login --username AWS --password-stdin <AWS_ACCOUNT_ID>.dkr.ecr.ap-northeast-2.amazonaws.com
 
 docker pull <AWS_ACCOUNT_ID>.dkr.ecr.<AWS_REGION>.amazonaws.com/<ECR_REPOSITORY>:<IMAGE_TAG>
-docker pull <AWS_ACCOUNT_ID>.dkr.ecr.<AWS_REGION>.amazonaws.com/<ECR_REPOSITORY>:ysocket
+docker pull <AWS_ACCOUNT_ID>.dkr.ecr.<AWS_REGION>.amazonaws.com/<ECR_REPOSITORY>:ysocket-dev
 
 docker run -d --name next-app -p 3000:3000 \
   -e ENV=<ENV> \
